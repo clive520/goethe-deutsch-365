5faed1dd6ae95eddc305fbab0685fe47468c7df8
clive520 <39232044+clive520@users.noreply.github.com>
2026-09-06 14:32:07 +0800

    Curriculum: Add Week 12 (Days 78-84) covering Business Emails, Office Telephone, Resume & Interview, Working Conditions, and German Workplace Culture

--- src/data/curriculumData.ts
+++ src/data/curriculumData.ts
@@ -9,6 +9,7 @@ import { week8Lessons } from './lessons/week8';
 import { week9Lessons } from './lessons/week9';
 import { week10Lessons } from './lessons/week10';
 import { week11Lessons } from './lessons/week11';
+import { week12Lessons } from './lessons/week12';
 
 export interface StageMetadata {
   id: CefrLevel;
@@ -789,6 +790,7 @@ const detailedLessons: Record<number, DayLesson> = {
   ...week9Lessons,
   ...week10Lessons,
   ...week11Lessons,
+  ...week12Lessons,
 };
 
 // 365-Day Generator that generates standard Goethe curriculum templates for all days 1..365
