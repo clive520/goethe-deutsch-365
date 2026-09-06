7419598a6ed9bc0b78c024a31d1ff0f726d16518
clive520 <39232044+clive520@users.noreply.github.com>
2026-09-06 14:29:23 +0800

    Curriculum: Add Week 11 (Days 71-77) completing Goethe A1 Exam Sprint (Days 71-75) and launching Stage A2 (Days 76-77)

--- src/data/curriculumData.ts
+++ src/data/curriculumData.ts
@@ -8,6 +8,7 @@ import { week7Lessons } from './lessons/week7';
 import { week8Lessons } from './lessons/week8';
 import { week9Lessons } from './lessons/week9';
 import { week10Lessons } from './lessons/week10';
+import { week11Lessons } from './lessons/week11';
 
 export interface StageMetadata {
   id: CefrLevel;
@@ -787,6 +788,7 @@ const detailedLessons: Record<number, DayLesson> = {
   ...week8Lessons,
   ...week9Lessons,
   ...week10Lessons,
+  ...week11Lessons,
 };
 
 // 365-Day Generator that generates standard Goethe curriculum templates for all days 1..365
